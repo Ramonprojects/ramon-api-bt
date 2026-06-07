74c1210c6ee5db3691422e255b71a9d2cfdae011
Ramonprojects <adm@ramonmago.com>
2026-06-06 22:10:02 -0300

    chore: shareCode UOI1O6JINLY -> MPZC2COIR8O (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/vqjid4fa?shareCode=UOI1O6JINLY&afp3=tap2&home=1" });
+    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/vqjid4fa?shareCode=MPZC2COIR8O&afp3=tap2&home=1" });
     res.end();
   }
